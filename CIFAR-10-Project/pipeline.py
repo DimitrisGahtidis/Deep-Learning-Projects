e7abebc8b63915d0d88b920f2303b988edb9e27c
Dimitris Gahtidis <DimitriGahtidis@gmail.com>
2022-02-04 16:01:59 +1100

    simplified accuracy calculations using confusion matrix

--- CIFAR-10-Project/pipeline.py
+++ CIFAR-10-Project/pipeline.py
@@ -1,7 +1,9 @@
+from sklearn.metrics import confusion_matrix
 import torch
 import torch.nn as nn
 from torch.nn import functional
 from torch.utils.data import Dataset, DataLoader
+from torch.utils.data.dataloader import default_collate
 import torchvision
 from torchvision import transforms
 from math import ceil
@@ -39,9 +41,7 @@ def train_model(n_epochs, learning_rate, train_dataloader, model, criterion, opt
     for epoch in range(n_epochs):
         for step, (sample_batch, label_batch) in enumerate(train_dataloader):
             
-            # flatten 28x28 image array to 784 column tensor and move to cuda device
-            sample_batch = sample_batch.to(device)
-            label_batch = label_batch.to(device)
+            sample_batch, label_batch = sample_batch.to(device), label_batch.to(device)
 
             # forward pass and loss
             label_pred = model(sample_batch)
@@ -55,7 +55,7 @@ def train_model(n_epochs, learning_rate, train_dataloader, model, criterion, opt
 
             # propagate changes
             optimizer.step()
-            if ((epoch+1) % 1 == 0)&((step+1) % 1000 == 0):
+            if ((epoch+1) % 1 == 0)&((step+1) % 50 == 0):
                 print(f'epoch: {epoch+1}/{n_epochs} batch = {step+1}/{n_steps} loss = {loss.item():.4f}')
     print("Finished training...")
 
@@ -85,8 +85,7 @@ def load_model(ModelClass, modelpath):
 def make_confusion_matrix(model, dataloader, classes, device):
     confusion = torch.zeros(len(classes), len(classes), device=device)
     for sample_batch, label_batch in dataloader:
-        sample_batch = sample_batch.to(device)
-        label_batch = label_batch.to(device)
+        sample_batch, label_batch = sample_batch.to(device), label_batch.to(device)
         sample_pred = model(sample_batch)
 
         _, batch_pred = torch.max(sample_pred, 1)
@@ -111,30 +110,28 @@ def plot_confusion_matrix(confusion, classes):
 
 ## implementing dataset transform, data is initially a PILImage of range [0,1] we want to normalize the data
 ## to a range of [-1, 1] after transforming it to a tensor 
-transform = torchvision.transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') # scan for cuda availability
+
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 train_dataset = torchvision.datasets.CIFAR10(root="./CIFAR-10-Project/CIFAR-10-Dataset", train=True, download=True, transform=transform)
 test_dataset = torchvision.datasets.CIFAR10(root="./CIFAR-10-Project/CIFAR-10-Dataset", train=False, transform=transform)
 
-batch_size = 10
-train_dataloader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
-test_dataloader = DataLoader(dataset=test_dataset, batch_size=batch_size)
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') # scan for cuda availability
+batch_size = 256
+train_dataloader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, pin_memory=True)
+test_dataloader = DataLoader(dataset=test_dataset, batch_size=batch_size, pin_memory=True)
 
 classes = ("plane", "car", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck")
 # model
 model = CNNModel().to(device)
 
 # loss and optimizer
-learning_rate = 0.005
-n_samples = len(train_dataset)
+learning_rate = 0.001
 criterion = nn.CrossEntropyLoss() # cross entropy contains softmax
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 # 3) training
-n_epochs = 15
-n_steps = ceil(n_samples/batch_size)
+n_epochs = 128
 
 train_model(n_epochs, learning_rate, train_dataloader, model, criterion, optimizer, device)
 
@@ -148,32 +145,18 @@ with torch.no_grad(): # Test accuracy
     plt.savefig("./CIFAR-10-Project/confusion_matrix.png", bbox_inches='tight')
 
     print("\nStarting accuracy test...")
-    n_correct = 0
-    n_samples = 0
-
-    n_correct_class = [0 for i in range(10)]
-    n_samples_class = [0 for i in range(10)]
-
-    for sample_batch, label_batch in test_dataloader:
-        sample_batch = sample_batch.to(device)
-        label_batch = label_batch.to(device)
-        sample_pred = model(sample_batch)
-
-        _, batch_pred = torch.max(sample_pred, 1)
-        n_samples = label_batch.shape[0]
-        n_correct = (batch_pred == label_batch).sum().item()
-
-        for prediction, label in zip(batch_pred, label_batch):
-            if (label == prediction):
-                n_correct_class[label] += 1
-            n_samples_class[label] += 1
-
-    acc = 100.0 * n_correct / n_samples
-    print(f'Total network accuracy = {acc}%')
-
-    for n_correct, n_samples, class_ in zip(n_correct_class, n_samples_class, classes):
+    for label, (row, class_) in enumerate(zip(confusion, classes)):
+        n_correct = row[label]
+        n_samples = row.sum()
         acc_class = 100.0 * n_correct/n_samples
-        rep = 100.0 * n_samples/sum(n_samples_class)
-        print(f'{class_} accuracy = {acc_class}% representation = {100*n_samples/sum(n_samples_class)}%')
+        rep = 100.0 * n_samples/torch.sum(confusion)
+        acc_class, rep = acc_class.item(), rep.item()
+        print(f'{class_} accuracy = {acc_class:.2f}% representation = {rep:.2f}%')
+
+    n_correct = torch.sum(torch.diag(confusion))
+    n_samples = torch.sum(confusion)
+    acc = 100.0 * n_correct / n_samples
+    print(f'Total network accuracy = {acc:.2f}%')
+
     plt.show()
 
