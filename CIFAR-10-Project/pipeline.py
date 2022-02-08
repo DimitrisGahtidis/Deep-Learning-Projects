16419ef320a7536123ff571af652afbef8a3954e
Dimitris Gahtidis <DimitriGahtidis@gmail.com>
2022-02-08 19:58:38 +1100

    Added functionality to read/write confusion matrices and loss at each epoch to validation_info.csv. In addition added function to retrieve class and total accuracy from a confusion matrix. Created a validation dataset and dataloader

--- CIFAR-10-Project/pipeline.py
+++ CIFAR-10-Project/pipeline.py
@@ -2,13 +2,14 @@ from sklearn.metrics import confusion_matrix
 import torch
 import torch.nn as nn
 from torch.nn import functional
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, random_split
 from torch.utils.data.dataloader import default_collate
 import torchvision
 from torchvision import transforms
-from math import ceil
+from math import ceil, floor
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 # Class and function definitions
 class CNNModel(nn.Module):
@@ -31,14 +32,24 @@ class CNNModel(nn.Module):
         sample = self.layer3(sample)
         return sample
 
-def train_model(n_epochs, learning_rate, train_dataloader, model, criterion, optimizer, device):
+
+
+
+def train_model(n_epochs, learning_rate, train_dataloader, model, criterion, optimizer, classes, device):
 
     batch_size = train_dataloader.batch_size
     n_samples = len(train_dataloader.dataset)
     n_steps = ceil(n_samples/batch_size)
 
     print("\nStarting training...")
-    for epoch in range(n_epochs):
+
+    open("./CIFAR-10-Project/validation_info.csv", "w").close() # clear file
+    confusion_dict = {}
+    confusion_dict["epoch"] = []
+    confusion_dict["loss"] = []
+    confusion_dict["matrices"] = []
+
+    for epoch in range(n_epochs): 
         for step, (sample_batch, label_batch) in enumerate(train_dataloader):
             
             sample_batch, label_batch = sample_batch.to(device), label_batch.to(device)
@@ -55,8 +66,17 @@ def train_model(n_epochs, learning_rate, train_dataloader, model, criterion, opt
 
             # propagate changes
             optimizer.step()
-            if ((epoch+1) % 1 == 0)&((step+1) % 50 == 0):
-                print(f'epoch: {epoch+1}/{n_epochs} batch = {step+1}/{n_steps} loss = {loss.item():.4f}')
+            # if ((epoch+1) % 1 == 0)&((step+1) % 256 == 0):
+        print(f'epoch: {epoch+1}/{n_epochs}, loss = {loss.item():.4f}')
+
+        with torch.no_grad():
+            confusion_dict["epoch"].append(epoch+1)
+            confusion_dict["loss"].append(loss.item())
+            confusion = make_confusion_matrix(model, validation_dataloader, classes, device)
+            confusion_dict["matrices"].append(confusion.tolist())
+            confusion_df = pd.DataFrame(confusion_dict)
+            confusion_df.to_csv("./CIFAR-10-Project/validation_info.csv")
+
     print("Finished training...")
 
 @torch.no_grad()
@@ -106,20 +126,40 @@ def plot_confusion_matrix(confusion, classes):
     plt.yticks(range(len(classes)), classes)
     plt.xticks(range(len(classes)), classes, rotation=90)
 
+
+def get_class_accuracy(confusion):
+    acc_list = []
+    for label, row in enumerate(confusion):
+        n_correct = row[label]
+        n_samples = row.sum()
+        acc_class = n_correct/n_samples
+        rep = n_samples/torch.sum(confusion)
+        acc_class, rep = acc_class.item(), rep.item()
+        acc_list.append(acc_class)
+    return acc_list
+
+def get_total_accuracy(confusion):
+    n_correct = torch.sum(torch.diag(confusion))
+    n_samples = torch.sum(confusion)
+    acc = n_correct/n_samples
+    return acc 
 # dataset and dataloader
 
 ## implementing dataset transform, data is initially a PILImage of range [0,1] we want to normalize the data
 ## to a range of [-1, 1] after transforming it to a tensor 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') # scan for cuda availability
 
-transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+transform = transforms.Compose([transforms.ToTensor(,device=device), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 train_dataset = torchvision.datasets.CIFAR10(root="./CIFAR-10-Project/CIFAR-10-Dataset", train=True, download=True, transform=transform)
 test_dataset = torchvision.datasets.CIFAR10(root="./CIFAR-10-Project/CIFAR-10-Dataset", train=False, transform=transform)
+test_dataset, validation_dataset = random_split(test_dataset, [ceil(len(test_dataset)/2), floor(len(test_dataset)/2)])
 
-batch_size = 256
+batch_size = 64
 train_dataloader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, pin_memory=True)
 test_dataloader = DataLoader(dataset=test_dataset, batch_size=batch_size, pin_memory=True)
+validation_dataloader = DataLoader(dataset=validation_dataset, batch_size=batch_size, pin_memory=True)
+
 
 classes = ("plane", "car", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck")
 # model
@@ -131,32 +171,26 @@ criterion = nn.CrossEntropyLoss() # cross entropy contains softmax
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 # 3) training
-n_epochs = 128
+n_epochs = 256
 
-train_model(n_epochs, learning_rate, train_dataloader, model, criterion, optimizer, device)
+train_model(n_epochs, learning_rate, train_dataloader, model, criterion, optimizer, classes, device)
 
 with torch.no_grad(): # Test accuracy
     modelpath = "./CIFAR-10-Project/model.pth"
     save_model(model, modelpath)
     # model = load_model(CNNModel, modelpath).to(device)
+
     confusion = make_confusion_matrix(model, test_dataloader, classes, device)
-    
     plot_confusion_matrix(confusion, classes)
     plt.savefig("./CIFAR-10-Project/confusion_matrix.png", bbox_inches='tight')
 
     print("\nStarting accuracy test...")
-    for label, (row, class_) in enumerate(zip(confusion, classes)):
-        n_correct = row[label]
-        n_samples = row.sum()
-        acc_class = 100.0 * n_correct/n_samples
-        rep = 100.0 * n_samples/torch.sum(confusion)
-        acc_class, rep = acc_class.item(), rep.item()
-        print(f'{class_} accuracy = {acc_class:.2f}% representation = {rep:.2f}%')
-
-    n_correct = torch.sum(torch.diag(confusion))
-    n_samples = torch.sum(confusion)
-    acc = 100.0 * n_correct / n_samples
-    print(f'Total network accuracy = {acc:.2f}%')
-
+    # print accuracy for each class
+    acc_list = get_class_accuracy(confusion)
+    for (acc, class_) in zip(acc_list, classes):
+        print(f"{class_} accruacy = {100*acc:.2f}%")
+    # print total accuracy
+    acc = get_total_accuracy(confusion)
+    print(f"\nTotal accruacy = {100*acc:.2f}%")
     plt.show()
 
